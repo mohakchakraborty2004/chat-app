aa377cba48aaf553e935928081b516592984f7de
mohak <c.mohak2004@gmail.com>
2024-11-19 01:13:49 +0530

    started the frontend, added ws instance, code is to be refactored

---
+++ hooks/useSocket.ts
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import WebSocket from "ws";
+
+export default function useSocket() {
+   
+    const [socket, setSocket] = useState<WebSocket | null>(null)
+
+    useEffect(()=> {
+        
+        const ws = new WebSocket("ws://localhost:8000")
+
+        ws.onopen = () => {
+            console.log("socket connected");
+            setSocket(ws);
+        }
+
+        ws.onclose = () => {
+            console.log("socket disconnected"); 
+            setSocket(null);
+        }
+    }, [])
+
+    return socket;
+
+}
